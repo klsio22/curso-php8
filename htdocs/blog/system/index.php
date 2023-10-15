386f1ea47c63523ea08f64bc3d6765b01033a5dc
Klesio dev <klesio.dev@gmail.com>
2023-10-15 00:34:10 -0300

    feat(project): added php with apache

--- htdocs/blog/system/index.php
+++ htdocs/blog/system/index.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-
-<body>
-
-<h1>ola mundo</h1>
-
-
-</body>
-
-</html>
+<?php
+echo "<h1>olá mundo</h1>";
+print "teste";
+phpinfo();
